05731e280969fb6684653eaf712caa50f8e98829
mastercoder202 <mzkchowdhury25@gmail.com>
2024-07-06 19:58:18 +0100

    myfirstcommit

---
+++ index.js
@@ -0,0 +1,10 @@
+const express=require("express")
+const port=3000
+const app=express();
+
+app.get("/login",(req,res)=>{
+    res.send("hello")
+})
+app.listen(port,()=>{
+    console.log("running")
+})
