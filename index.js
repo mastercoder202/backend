7bcf3b6440d9ab59641c41cb39ff21e5b951e654
mastercoder202 <mzkchowdhury25@gmail.com>
2024-07-06 20:16:15 +0100

    my second commit

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require("express")
 const port=3000
 const app=express();
 
-app.get("/login",(req,res)=>{
+app.get("login",(req,res)=>{
     res.send("hello")
 })
 app.listen(port,()=>{
